f23720f68e23846130aeab5733e66bea3162c2be
Juliano <julianotrabalho2016@gmail.com>
2019-10-08 14:40:03 -0300

    UPDATE - query and route params

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const server = express();
 
+// query params = ?/teste=1
+// route params = /teste/1
+// request body = {"nome": "Juliano", "email": "julianodeveloper2018@gmail.com"}
+
 server.get("/teste", (req, res) => {
-  return res.json({ text: "Hello World" });
+  const nome = req.query.nome;
+  return res.json({ text: `Ola ${nome}` });
 });
 
 server.listen(3000);
